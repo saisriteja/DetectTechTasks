0d50414cfb26ebfaa37f7f5debb2940e7f23e7bb
saisriteja <kuppasaisriteja@gmail.com>
2022-07-12 11:10:15 +0530

    yolo

---
+++ DetectTechTask5/scripts/dataset_download.py
@@ -0,0 +1,32 @@
+import gdown
+
+import os
+
+# os.mkdir('/dataset')
+# https://drive.google.com/file/d/1YM4w1FP0gWbe_sTd_A0j2njtZLtD0SUw/view?usp=sharing
+
+
+
+# id = '1YM4w1FP0gWbe_sTd_A0j2njtZLtD0SUw'
+# https://drive.google.com/file/d/1UA3BBXQcKh0zAwJYwVTTX0i3wE_Fd43y/view?usp=sharing
+# https://drive.google.com/file/d/17EAQ-L7BYZqIX5_APKHArcUvddkMIWzG/view?usp=sharing
+# https://drive.google.com/file/d/1BwiWGzU3njVxt-0Lm4smcEWDOnM8oHaa/view?usp=sharing
+
+
+url = 'https://drive.google.com/uc?id=1BwiWGzU3njVxt-0Lm4smcEWDOnM8oHaa'
+output = 'val.zip'
+gdown.download(url, output, quiet=False)
+
+
+# url = 'https://drive.google.com/uc?id=1a2oHjcEcwXP8oUF95qiwrqzACb2YlUhn'
+# output = 'train.zip'
+# gdown.download(url, output, quiet=False)
+
+
+# url = 'https://drive.google.com/uc?id=1bxK5zgLn0_L8x276eKkuYA_FzwCIjb59'
+# output = 'val.zip'
+# gdown.download(url, output, quiet=False)
+
+# url = 'https://drive.google.com/uc?id=1PFdW_VFSCfZ_sTSZAGjQdifF_Xd5mf0V'
+# output = 'test.zip'
+# gdown.download(url, output, quiet=False)
